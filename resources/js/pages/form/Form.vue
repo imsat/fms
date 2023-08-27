5f9f94c34993f455427aa48654687d3f6cac2065
imsat <satyajit.cse@yahoo.com>
2023-08-27 19:02:34 +0600

    update

--- resources/js/pages/form/Form.vue
+++ resources/js/pages/form/Form.vue
@@ -1,5 +1,49 @@
 <template>
-<h2>Feedback Form</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+        <h4 class="text-body-emphasis">
+            Feedback Form
+            <a href="" class="btn btn-sm btn-primary float-end">Add New</a>
+        </h4>
+
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table w-100">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First</th>
+                                <th>Last</th>
+                                <th>Handle</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th>2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th>3</th>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@twitter</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
